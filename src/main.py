8e011a74182196a8dea5057c622f6cc457ca3d53
deandresugandhi <deandre_21@hotmail.com>
2023-11-16 23:45:58 +1100

    Convert to ttf

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 The main module from which the program will be executed.
 """
 # Standard Library Modules
+import os
 import platform
 
 # Third-party Library Modules
@@ -16,11 +17,11 @@ from app_gui import AppGui
 
 # Add font files that will be used and functionalities to ensure font works
 pyglet.options['win32_gdi_font'] = True
-pyglet.font.add_file('./assets/fonts/Moon_Bold.otf')
-pyglet.font.add_file('./assets/fonts/Moon_Light.otf')
-pyglet.font.add_file('./assets/fonts/Moon2.0-Bold.otf')
-pyglet.font.add_file('./assets/fonts/Moon2.0-Light.otf')
-pyglet.font.add_file('./assets/fonts/Moon2.0-Regular.otf')
+pyglet.font.add_file(os.path.join('.', 'assets', 'fonts', 'Moon_Bold.ttf'))
+pyglet.font.add_file(os.path.join('.', 'assets', 'fonts', 'Moon_Light.ttf'))
+pyglet.font.add_file(os.path.join('.', 'assets', 'fonts', 'Moon2.0-Bold.ttf'))
+pyglet.font.add_file(os.path.join('.', 'assets', 'fonts', 'Moon2.0-Light.ttf'))
+pyglet.font.add_file(os.path.join('.', 'assets', 'fonts', 'Moon2.0-Regular.ttf'))
 
 # Set GUI base
 root = customtkinter.CTk()
@@ -34,7 +35,7 @@ root.iconphoto(False, photo_image)
 root.wm_iconphoto(True, photo_image)
 
 if platform.system() == "Windows":
-    root.iconbitmap('./assets/chordtranslatorlogo.ico')
+    root.iconbitmap(os.path.join('.', 'assets', 'chordtranslatorlogo.ico'))
 
 # Set GUI theme
 customtkinter.set_appearance_mode("dark")
